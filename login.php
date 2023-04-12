a532c9bc47702e28fa9d7146105575c381a84326
kabil <128507560+kabilg143@users.noreply.github.com>
2023-04-12 10:25:31 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,71 @@
+      
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.container {
+  padding: 16px;
+}
+
+div{
+  width: 30%;
+  background-color:skyblue;
+}
+
+</style>
+</head>
+<body>
+<center>
+
+<h2 style="color:darkblack">Login Form</h2>
+</center>
+
+
+<form action="query.php" method="post">
+  
+ <center>
+  <div class="container">
+    <label for="name"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="name" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="pwd" required>
+        
+    <button type="submit" name="login_submit">Login</button>
+     <p align="right"><a href="register.php" >register</a> </p>
+  </div>
+</center>
+  
+</form>
+
+</body>
+</html>
+
