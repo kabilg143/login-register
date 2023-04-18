e06f92c1b64b0f2eb02f96b62e4a0cc351e2ddf3
kabil <128507560+kabilg143@users.noreply.github.com>
2023-04-18 11:43:12 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+
+include "header.php";
+ include "navigation.php";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<style>
+     body{
+            margin-top:16px;
+        }
+</style>
+</head>
+<body >
+
+
+
+
+  <center><h2 style="color:red" ><b>HOME PAGE</b></h2></center>
+
+  
+  <?php include "footer.php"; ?>
+
+</body>
+</html>
+
+
