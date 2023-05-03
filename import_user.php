af79d6173677205d52926e16298b2cef3aafdbdf
kabil <128507560+kabilg143@users.noreply.github.com>
2023-05-03 19:49:12 +0530

    Add files via upload

---
+++ import_user.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>import</title>
+</head>
+<body>
+    <form action="import_user_query.php" method="post" enctype="multipart/form-data">
+       <strong>Select file:</strong> <br>
+       <input type="file" name="xmlfile"> <br> <br>
+       <input type="submit" value="import" >
+    </form>
+
+</body>
+</html>
+
+
