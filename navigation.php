6b84dea34800996e4ba4a2625695a6bdccb6dbc4
kabil <128507560+kabilg143@users.noreply.github.com>
2023-05-09 12:06:57 +0530

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navigation bar</title>
+
+
+    <style>
+           
+         body 
+         {
+            margin: 0;
+         }
+        ul 
+        {
+            list-style-type: none;
+            margin:-10px;
+            margin-top:-16px;
+            padding-right:4px;
+            padding:0;
+            width: 10%;
+            background-color:grey;
+            position: fixed;
+            height:100%;
+  
+        }
+
+        li a{
+            display:block;
+            text-decoration: none;
+            padding: 4px 12px;
+            color:white;
+
+        }
+ 
+        li a:hover {
+            background-color:#009b77;
+            color:aqua;
+            text-decoration: none;
+        }
+
+    </style>
+</head>
+<body>
+  
+
+<ul>
+  <li><a  href="add_users_form.php">Add User</a></li>
+  <li><a href="display_users.php">Display Users</a></li>
+  <li><a href="import_user.php">import user</a></li>
+  <li><a href="import_ajax.php">import using ajax</a></li>
+</ul>
+
+</body>
+</html>
